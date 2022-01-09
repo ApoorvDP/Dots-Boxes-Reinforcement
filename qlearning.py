f43b15f91942ba9f4ff31d6aa4b95f78ba5bf033
Apoorv Pandey <27015073+ApoorvDP@users.noreply.github.com>
2022-01-08 23:56:52 -0500

    test results with DQN

--- qlearning.py
+++ qlearning.py
@@ -43,6 +43,7 @@ def train_Qnet(epsilon_greedy_F, valid_moves_F, make_move_F, boxes_created_F, pa
     Qnet = nn.NN(False, 48, parameters['network'], 1, parameters['use_ReLU'])
     outcomes, repk = np.zeros(parameters['epochs']*parameters['games_per_epoch']), -1
     for epoch in range(parameters['epochs']):
+        start_time = time.time()
         if epoch > 0:
             ep *= parameters['epsilon_decay_factor'] # Degree of randomness of move as a fraction => by default ep=1 for epoch 0, i.e. 100% random move
         samples, epsilon = [], max(parameters['min_epsilon'], ep) # Minimum probability of a random move is min_epsilon
@@ -79,7 +80,7 @@ def train_Qnet(epsilon_greedy_F, valid_moves_F, make_move_F, boxes_created_F, pa
         X, T = samples[:, :48], samples[:, 48:49]+samples[:, 49:50] # Training inputs and target values for the neural network
         Qnet, _ = Qnet.train_pytorch(X, T, parameters['updates_per_epoch'], X.shape[0], parameters['learning_rate'], parameters['use_SGD']) # Training the neural network
         if verbose:
-            print(f'(Epoch: {epoch+1}, Mean Outcome: {outcomes.reshape(-1, parameters["games_per_epoch"])[epoch, :].mean():.2f}, Epsilon: {epsilon:.2f})')
+            print(f'(Epoch: {epoch+1}, Win %: {(round(outcomes.reshape(-1, parameters["games_per_epoch"])[epoch, :].mean(), 2)+1)*100/2:.1f}, Epsilon: {epsilon:.2f}), Time taken: {time.time() - start_time:.2f}s')
     return Qnet, outcomes
 
 def test_Qnet(epsilon_greedy_F, valid_moves_F, make_move_F, boxes_created_F, num_tests, runs, num_games, Qnet, Qnet_adv, ep=0, verbose=False):
